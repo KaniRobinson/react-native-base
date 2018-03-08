ea6e4a05eef7b29a2ce9b3474ee7db92ecf0a365
KaniRobinson <me@kanirobinson.co.uk>
2018-03-08 09:31:07 +0000

    Added Login Implementation, TODO Error Handling now

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,15 +1,37 @@
-import axios from 'axios';
-import { USER_LOGIN, USER_REGISTER } from './types';
-
+import { USER_LOGIN, USER_REGISTER } from './types'
+import { Actions } from 'react-native-router-flux'
+import Authentication from '../api/Authentication'
+import Method from '../api/Method'
 
 export const userLogin = (email, password) => {
-    return {
-        type: USER_LOGIN,
-        payload: {
-            email,
-            password
+    let storage = new Method.Storage(),
+        dispatch = {
+            type: USER_LOGIN,
+            payload: {
+                success: false,
+                data: null
+            }
         }
-    }
+
+    new Authentication.Login(email, password)
+        .then(response => {
+            if(response.data.error == 0) {
+                storage.set('@User:isAuthenticated', true)
+                .then((value) => {
+                    dispatch.payload.success = true
+                    Actions.dashboard()
+                })
+            } else {
+                if(response.data.message == 'failure') {
+                    dispatch.payload.data = response.data.data
+                }
+            }
+        })
+        .catch(error => {
+            dispatch.payload.data.System.unexpected = 'Unexpected Error. Please try again later.'
+        })
+
+        return dispatch
 }
 
 export const userRegister = (username, first_name, last_name, email, nhs_number, phone_number, password, confirm_password) => {
